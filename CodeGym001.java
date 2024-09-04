727b2a40132958d8966fd02eec5980ff3fb4395b
Aleksander <alek@siudek.net>
2024-09-04 13:28:34 +0200

    WIP

---
+++ CodeGym001.java
@@ -0,0 +1,7 @@
+public class CodeGym001 {
+    public static void main(String[] args) {
+        String imie="Aleksander";
+        System.out.println(" " +imie);
+        
+    }
+}
---
+++ CodeGym002.java
@@ -0,0 +1,3 @@
+public class CodeGym002 {
+    
+}
