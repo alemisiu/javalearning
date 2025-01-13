f4e822a3c5fdb90211a118c5505528bb8257b69c
Aleksander <alek@siudek.net>
2025-01-13 13:25:41 +0100

    wip

---
+++ CodeGym030.java
@@ -0,0 +1,26 @@
+
+ public class CodeGym030 {
+
+    private String imie;
+    private int  wiek;
+    private char plec;
+
+
+public void inicjalizuj(String imie){
+    this.imie=imie;
+}
+
+public void inicjalizuj(String imie, int wiek){
+    this.imie = imie;
+    this.wiek = wiek;
+}
+public void inicjalizuj(String imie, int wiek, char plec){
+    this.imie = imie;
+    this.wiek=wiek;
+    this.plec=plec;
+}
+
+    public static void main(String[] args) {
+
+    }
+}
