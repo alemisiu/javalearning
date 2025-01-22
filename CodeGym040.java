41c1b00a12634d1adede94ef8be95f15147bb548
Aleksander <alek@siudek.net>
2025-01-22 12:46:37 +0100

    WIP

---
+++ CodeGym040.java
@@ -0,0 +1,12 @@
+public class CodeGym040 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        int suma = a + b;
+        System.out.println("Suma = " + suma);
+    }
+}
+
+    
