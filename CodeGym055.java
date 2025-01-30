6a58842b02bde648e1e213c4f79244ab653c5c9f
Aleksander <alek@siudek.net>
2025-01-30 17:08:12 +0100

    WIP

---
+++ CodeGym055.java
@@ -0,0 +1,39 @@
+// import java.util.ArrayList;
+
+// /* 
+// Statyczne koty
+// */
+// class CodeGym055{
+//  public static ArrayList<CodeGym055> koty = new ArrayList<>();
+
+//     private static int counter = 1;
+
+//     public static int licznikKotow;
+//     private String name;
+
+//     // Konstruktor bezargumentowy tworzący kota i automatycznie dodający go do listy koty
+//     public CodeGym055() {
+//         this.name = "Kot " + counter++;
+//         koty.add(this);
+//     }
+
+//     // Metoda do wyświetlania wszystkich kotów z listy
+//     public static void printKoty() {
+//         for (CodeGym055 CodeGym055 : koty) {
+//             System.out.println(CodeGym055);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         // Tworzenie 10 obiektów Kot
+//         for (int i = 0; i < 10; i++) {
+//            var CodeGym055 = new Kot();
+//             koty.add(CodeGym055);
+           
+//         }
+
+//         // Wyświetlenie listy kotów
+//         CodeGym055.printKoty();
+        
+//     }
+// }
