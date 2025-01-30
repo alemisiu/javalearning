ace7c815e6c5034cced453a4ae3c66c545fc1407
Aleksander <alek@siudek.net>
2025-01-30 18:34:13 +0100

    WIP

---
+++ CodeGym060.java
@@ -0,0 +1,17 @@
+public class CodeGym060 {
+    public static String imie;
+
+ 
+
+    public static class Kot {
+       public static String imie;
+     }
+ 
+     public static class Pies {
+        public static String imie;
+     }
+ 
+     public static void main(String[] args) {
+ 
+     }
+ }
