c499dd426f065f18231b2856abba6ac0c155979e
Aleksander <alek@siudek.net>
2025-01-30 18:34:28 +0100

    WIP

--- CodeGym059.java
+++ CodeGym059.java
@@ -1,3 +1,11 @@
 public class CodeGym059 {
+
+        public static void main(String[] args) {
+            // tutaj wpisz swój kod
+            PocalujMojBlyszczacyMetalowyTylnySilownik obiekt = new PocalujMojBlyszczacyMetalowyTylnySilownik();
+            System.out.println(obiekt);
+        }
     
-}
+        public static class PocalujMojBlyszczacyMetalowyTylnySilownik {
+        }
+    }
