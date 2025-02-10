96c5dcebac8d44a6b7102a4bf3dc43c01ccc3a08
Aleksander <alek@siudek.net>
2025-02-10 23:25:52 +0100

    WIP

---
+++ CodeGym062.java
@@ -0,0 +1,32 @@
+
+   
+
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    
+    /* 
+    Maksimum w tablicy
+    */
+    
+    public class CodeGym062{
+        public static void main(String[] args) throws Exception {
+            int[] tablica = inicjalizujTablice();
+            int max = max(tablica);
+            System.out.println(max);
+        }
+    
+        public static int[] inicjalizujTablice() throws IOException {
+            // Tworzy i zapełnia tablicę
+               BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+    
+            
+            return null;
+        }
+    
+        public static int max(int[] tablica) {
+            // Znajduje maksimum
+            return 0;
+        }
+    }
+    
