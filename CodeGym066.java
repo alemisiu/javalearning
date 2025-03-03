5a2ebba504651d97fe5c63c6c117e2d0387b2780
Aleksander <alek@siudek.net>
2025-03-03 09:05:25 +0100

    WIP

---
+++ CodeGym066.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CodeGym066 {
+    public static void main(String[] args) {
+          BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] tablica = new int[15];
+        
+    }
+
+}
